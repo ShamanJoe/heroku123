4c08e30c8216e4f7c6e254f52a1434a572614b65
ShamanJoe <jonathansolheim@outlook.com>
2022-04-02 18:14:06 +0200

    React router

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,30 @@
 import React from "react"
 import * as ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"))
+function FrontPage() {
+    return <div>
+        <h1>Movie database exam</h1>
+            <ul>
+                <li><Link to={ListMovies}>List of all movies</Link></li>
+            </ul>
+    </div>
+}
+
+function ListMovies() {
+    return <div>
+        <h1>List of all movies</h1>
+    </div>;
+}
+
+function Application() {
+    return <BrowserRouter>
+       <Routes>
+           <Route path={"/"} element={<FrontPage/>}/>
+           <Route path={"/movies"} element={<ListMovies/>}/>
+       </Routes>
+
+    </BrowserRouter>
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"))
