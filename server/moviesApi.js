49b57192508f7ad91548abdf3465356a8778c43b
ShamanJoe <jonathansolheim@outlook.com>
2022-04-15 15:29:21 +0200

    MongoDB try #1 (not succesful)

---
+++ server/moviesApi.js
@@ -0,0 +1,26 @@
+import {Router} from "express";
+
+export function MoviesApi(mongoDatabase) {
+    const router = new Router();
+
+    router.get("/", async(req, res) => {
+        const movies = await mongoDatabase.collection("movies")
+            .find()
+            .toArray()
+        res.json(movies)
+    });
+
+    router.post("/new", (req, res) => {
+        res.sendStatus(500)
+    });
+    return router;
+}
+const movies = [
+    {
+        title: "Movie 1"
+    },
+    {
+        title: "Movie 2"
+    },
+
+];
--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 import express from "express";
 import * as path from "path";
+import {MoviesApi} from "./moviesApi.js";
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
 
+dotenv.config()
 
 const app = express();
 app.use(express.static("../client/dist"));
+
+const mongoClient = new MongoClient(process.env.MONGODB_URL)
+await mongoClient.connect().then(async() =>{
+    console.log("Connected to mongodb");
+    const database = await mongoClient.db().admin().listDatabases();
+    console.log(database);
+    app.use("/api/movies", MoviesApi(mongoClient.db("pg6301eksamen")))
+})
+
+
+
 
 app.use((req, res, next) => {
     if(req.method === "GET" && !req.path.startsWith("/api")){
